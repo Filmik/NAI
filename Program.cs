827d153e982f6f8217163e7138a306bc4ce3e105
Filip Mikołajek <s16586@pjwstk.edu.pl>
2019-05-07 14:26:01 +0200

    Aktualizacja-1

--- Program.cs
+++ Program.cs
@@ -1,40 +1,43 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DesktopApp1
 {
     static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+
+        {// Get a handle to an application window.
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        // Send a series of key presses to the Calculator application.
+        private void button1_Click(object sender, EventArgs e)
         {
-           //do osobnej klacy
-           /*private*/ void jaki_klawisz(object sender/*co to za typ*/,KeyEventArgs e)
+            // Get a handle to the Calculator application. The window class
+            // and window name were obtained using the Spy++ tool.
+            IntPtr calculatorHandle = FindWindow("CalcFrame", "Calculator");
+
+            // Verify that Calculator is a running process.
+            if (calculatorHandle == IntPtr.Zero)
             {
-                if (e.KeyValue == (char)Keys.A)
-                {
-                    //zamiast wiaomosci to zapisz do tebeli zeby dziedziczylo
-                    MessageBox.Show("Klawisz A");
-                }
-                if (e.KeyValue == (char)Keys.W)
-                {
-                    MessageBox.Show("Klawisz W");
-                }
-                if (e.KeyValue == (char)Keys.S)
-                {
-                    MessageBox.Show("Klawisz S");
-                }
-                if (e.KeyValue == (char)Keys.D)
-                {
-                    MessageBox.Show("Klawisz D");
-                }
+                MessageBox.Show("Calculator is not running.");
+                return;
             }
+
+            // Make Calculator the foreground application and send it 
+            // a set of calculations
+
+            // [STAThread]
+            static void Main()
+        {
+                SetForegroundWindow(calculatorHandle);
+                SendKeys.Send("{D}");
+            //void jaki_klawisz();
         }
     }
 }
---
+++ Tworzenie_Osobnika.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp1
+{
+    class Tworzenie_Osobnika
+    {
+        /*private*/
+        void jaki_klawisz(object sender/*co to za typ, KeyEventArgs e*/)
+        {
+            // Get a handle to an application window.
+        /* [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName,string lpWindowName);*/
+
+        int x;//kolor pikseli konczoncy pentle
+            for (x = 100; x == 0; x--)
+            {
+                Random random = new Random();//jak to się nazywało i do czego słuzylo
+                int num = random.Next(3);
+
+                if (num > 1)
+                {
+                    SendKeys.Send("{D}");
+                } else
+                {
+                    SendKeys.Send("{W}");
+                }
+
+                /* if (e.KeyValue == (char)Keys.D)
+                 {
+                     MessageBox.Show("Klawisz D");
+                 }*/
+            }
+        }
+    }
+}
